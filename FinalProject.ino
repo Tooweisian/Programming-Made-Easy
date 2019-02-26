78750f63e6be14139c47ba673e46e0efd27978e0
Tooweisian <47927931+Tooweisian@users.noreply.github.com>
2019-02-25 19:41:13 -0500

    Add files via upload

--- FinalProject.ino
+++ FinalProject.ino
@@ -1,16 +1,23 @@
+/*Wei-Jie Zhou, MYP Project 2018-2019 Class
+ * LC A 6th Hour Interdisc
+ * Start of Program
+*/
+
+//This is a Library. Libraries are prewritten code to perform a certain task. The Library <SoftwareSerial.h> Allows the car itself to talk with the phone and computer.
 #include <SoftwareSerial.h>
 
 //Variables
 //https://www.arduino.cc/en/Reference/SoftwareSerialRead
 SoftwareSerial BT(10, 11); // RX | TX
 
-//Motor A
+//These Pins are the input and output; This is used to control the direction of each motor. I have indicated where each motor is located in the board.
+//Motor A; Bottom RIGHT Wheel
 const int motorPin1  = 6; // Pin 14 of L293
 const int motorPin2  = 7; // Pin 10 of L293
-//Motor B
+//Motor B; Bottom LEFT Wheel
 const int motorPin3  = 9; // Pin  7 of L293
 const int motorPin4  = 8; // Pin  2 of L293
-//Motor C
+//Motor C; Top LEFT Wheel
 const int motorPin5  = 2; //Pin 14 of L293
 const int motorPin6  = 3; //Pin 10 of L293
 //Motor D
@@ -41,45 +48,45 @@ void setMotorPower() {
     if ( input == '1' ) {
         digitalWrite(motorPin1, LOW);
         digitalWrite(motorPin2, HIGH);
-        digitalWrite(motorPin3, LOW);
-        digitalWrite(motorPin4, HIGH);
+        digitalWrite(motorPin3, HIGH);
+        digitalWrite(motorPin4, LOW);
         digitalWrite(motorPin5, HIGH);
         digitalWrite(motorPin6, LOW);
-        digitalWrite(motorPin7, HIGH);
-        digitalWrite(motorPin8, LOW);
+        digitalWrite(motorPin7, LOW);
+        digitalWrite(motorPin8, HIGH);
     }
     // 2, reverse motor 
     else if ( input == '2') {
-        digitalWrite(motorPin1, LOW);
-        digitalWrite(motorPin2, HIGH);
+        digitalWrite(motorPin1, HIGH);
+        digitalWrite(motorPin2, LOW);
         digitalWrite(motorPin3, LOW);
         digitalWrite(motorPin4, HIGH);
         digitalWrite(motorPin5, LOW);
         digitalWrite(motorPin6, HIGH);
-        digitalWrite(motorPin7, LOW);
-        digitalWrite(motorPin8, HIGH);     
+        digitalWrite(motorPin7, HIGH);
+        digitalWrite(motorPin8, LOW);   
     }
     //else if BT reads 3, turn left
     else if (input == '3') {
         digitalWrite(motorPin1, LOW);
         digitalWrite(motorPin2, HIGH);
-        digitalWrite(motorPin3, HIGH);
-        digitalWrite(motorPin4, LOW);
+        digitalWrite(motorPin3, LOW);
+        digitalWrite(motorPin4, HIGH);
         digitalWrite(motorPin5, LOW);
         digitalWrite(motorPin6, HIGH);
-        digitalWrite(motorPin7, HIGH);
-        digitalWrite(motorPin8, LOW);
+        digitalWrite(motorPin7, LOW);
+        digitalWrite(motorPin8, HIGH);
     }
     //else if BT read 4, turn right
     else if (input == '4') {
         digitalWrite(motorPin1, HIGH);
         digitalWrite(motorPin2, LOW);
-        digitalWrite(motorPin3, LOW);
-        digitalWrite(motorPin4, HIGH);
+        digitalWrite(motorPin3, HIGH);
+        digitalWrite(motorPin4, LOW);
         digitalWrite(motorPin5, HIGH);
         digitalWrite(motorPin6, LOW);
-        digitalWrite(motorPin7, LOW);
-        digitalWrite(motorPin8, HIGH);
+        digitalWrite(motorPin7, HIGH);
+        digitalWrite(motorPin8, LOW);
     }
 }
  
